d6eb2695bacfe89afc1ded741a5db32255b1db8d
Kinimod12 <dominik.bilski42@gmail.com>
2022-05-09 22:09:05 +0200

    (FE) Add register form.

--- communicator-frontend/src/app/app.module.ts
+++ communicator-frontend/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {FormsModule} from "@angular/forms";
+import {authInterceptorProviders} from "./helpers/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -22,9 +24,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- communicator-frontend/src/app/register/register.component.ts
+++ communicator-frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  }
+  registerSuccessful = false;
+  signUpFailed = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const {username, email, password} = this.form;
+    this.authService.register(username, email, password).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.registerSuccessful = true;
+          this.signUpFailed = false;
+        },
+        error: (err) => {
+          this.errorMessage = err.error.message;
+          this.signUpFailed = true;
+        }
+      }
+    )
   }
 
 }
